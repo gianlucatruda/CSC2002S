12ac81298ef0d496757921f17cfe3dbc1135cac8
Gianluca Truda <gianlucatruda@gmail.com>
2016-07-27 22:28:20 +0200

    Updated MergesortParallel to feature some skeleton code which has a working serial instance that utilises the Arrays.sort() method provided by Java itself. The override in MergesortParallel as well as the compute() method are both functional and, if only working sequentially, completes a successful sort.

--- src/MergesortParallel.java
+++ src/MergesortParallel.java
@@ -1,10 +1,46 @@
+import java.util.Arrays;
 import java.util.concurrent.*;
 
 /**
- *
+ * A Mergesort algorithm that uses Java's lightweight threads from the Fork/Join Framework
+ * to implement parallelism for the purpose of speedup.
  * @author gianlucatruda
  * Created 26 July 2016
  */
 public class MergesortParallel extends RecursiveAction{
+    //Declaration of the private variables for a Mergesort object
+    private int[] arr;
+    private int begin, end;
+    private int thresh;
+    
+    //Constructor method for MergesortParallel
+    public MergesortParallel(int[] a, int begin, int end, int threshold){
+        this.arr = a;
+        this.begin = begin;
+        this.end = end;
+        this.thresh = threshold;
+    }
+    
+    
+    //Override for the compute() method implemented from RecursiveAction (main body of code)
+    @Override
+    protected void compute() {
+        //If statement to catch the instance where the threshold has been reached and to perform a sequential sort.
+        if(end - begin <= thresh){
+            //A system-implemented sequential sort over the range provided.
+            Arrays.sort(arr, begin, end);
+        }
+        else{
+            //Parallel code to sort the two halves in parallel
+            
+            
+        }
+        
+        
+        
+        //Sequential merge of the two halves
+        
+        
+    }
     
 }
