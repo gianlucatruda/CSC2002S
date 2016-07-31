98d1a88cfd2b4c7f4cc0fdf33addcd8075e8e916
Gianluca Truda <gianlucatruda@gmail.com>
2016-07-31 18:54:36 +0200

    Worked at the quicksort. It now runs without stack overflow, but is incorrect.

--- src/QuicksortParallel.java
+++ src/QuicksortParallel.java
@@ -12,17 +12,16 @@ import java.util.Random;
 public class QuicksortParallel extends RecursiveAction{
     //Declaration of private variables for a Quicksort object
     private int[] arr;
-    private int[] clone;
     private final int begin, end, thresh;
     private int pivot;
     
     //Constructor method
-    public QuicksortParallel(int[] a, int begin, int end, int thresh){
+    public QuicksortParallel(int[] a, int thresh){
         this.arr = a;
-        this.begin = begin;
-        this.end = end;
+        this.begin = 0;
+        this.end = arr.length;
         this.thresh = thresh;
-        this.pivot = (end-begin)/2;
+        this.pivot = (end-begin)/2 +1;
 }
     //Override for the compute() method implemented from RecursiveAction
     @Override
@@ -34,57 +33,53 @@ public class QuicksortParallel extends RecursiveAction{
         else{
             //this.pivot = getMedian();
             //Perform a parallel pack
-            this.pack(begin, end, arr[pivot]);
+            this.pack(arr[pivot]);
         }
     }
 
     /**
      * A pack algorithm using the bit-bitsum-output method to enable parallelization.
      */
-    private void pack(int start, int end, int val){
-        int[] bits = new int[(end-start)+1];
-        int[] bitsum = new int[bits.length];
-        for (int i = start; i <=end; i++) {
+    private void pack(int val){
+        int[] bits = new int[end+1];
+        int[] bitsum = new int[end+1];
+        for (int i = 0; i <end; i++) {
             if(arr[i]>= val){
-                bits[i-start] = 1;
+                bits[i] = 1;
             }
             else if(arr[i]<val){
-                bits[i-start] = 0;
+                bits[i] = 0;
             }
         }
         
         //Parallelised Prefix-Sum Algorithm
-        PrefixSumParallel pfp = new PrefixSumParallel(bits, bitsum, start, end, null, 4, true);
+        PrefixSumParallel pfp = new PrefixSumParallel(bits, bitsum, begin, end, null, 4, true);
         invokeAll(pfp);
         pfp.apply();
         
         //Create and populate the array of items creater than pivot value
         int[] right = new int[bitsum[bitsum.length-1]];
-        int[] left = new int[(end-start)+1-right.length];//(end-start)-bitsum[bitsum.length-1]+1];
+        int[] left = new int[(end)-right.length];
         int count = 0;
-        for (int i = 0; i < bitsum.length; i++) {
+        for (int i = 0; i <arr.length; i++){          
+            //System.out.println("i: "+i+"\tbits[i]: "+bits[i]+"\tarr[i]: "+arr[i]+"\tcount: "+count+"\tleft.len: "+left.length);
             if(bits[i]==1){
-                right[bitsum[i]-1] = arr[start + i];
+                right[bitsum[i]-1] = arr[i];
             }
             else{
-                //left[count++] = arr[start+i];
+                left[count++] = arr[i];
             }
         }
-        System.out.println(Arrays.toString(right));
-        System.out.println(Arrays.toString(left));
+        System.out.println(Arrays.toString(left)+"-"+val+"-"+Arrays.toString(right));
         
-        invokeAll(  new QuicksortParallel(left, begin, pivot, thresh),
-                    new QuicksortParallel(right, pivot, end, thresh));
+        invokeAll(  new QuicksortParallel(left, thresh),
+                    new QuicksortParallel(right, thresh));
         
         int[] out = new int[left.length+right.length];
-        for (int i = 0; i < left.length; i++) {
-            out[i] = left[i];
-        }
-        for (int i = 0; i < right.length; i++) {
-            out[i+left.length] = right[i];
-        }
+        System.arraycopy(left, 0, out, 0, left.length);
+        System.arraycopy(right, 0, out, left.length, right.length);
         
-        System.out.println(Arrays.toString(out));
+        this.arr = out.clone();
     }
     
  
