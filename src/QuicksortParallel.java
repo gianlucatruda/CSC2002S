3ab0f2945f5ff854ebf7c2f0fa9c041d6c9a61cd
Gianluca Truda <gianlucatruda@gmail.com>
2016-07-31 15:47:21 +0200

    Updated QuicksortParallel to begin incorporating parallelization of both the recursive and partitioning steps — added a pack method skeleton as well as a method to estimate the median value and return its index for the purpose of selecting a pivot. Fleshed out the TestQuicksortParallel class to apply basic JUnit tests to the quicksort — mostly identical to the test for the mergesort.

--- src/QuicksortParallel.java
+++ src/QuicksortParallel.java
@@ -1,9 +1,11 @@
 import java.util.Arrays;
 import java.util.concurrent.*;
+import java.util.Random;
 
 /**
  * A quicksort algorithm that uses Java's lightweight threads from the Fork/Join Framework
  * to implement parallelism for the purpose of speedup.
+ * Both the recursive call step AND the partitioning step are parallelized!
  * @author gianlucatruda
  * Created 26 July 2016
  */
@@ -12,6 +14,7 @@ public class QuicksortParallel extends RecursiveAction{
     private int[] arr;
     private int[] clone;
     private final int begin, end, thresh;
+    private int pivot;
     
     //Constructor method
     public QuicksortParallel(int[] a, int begin, int end, int thresh){
@@ -19,14 +22,69 @@ public class QuicksortParallel extends RecursiveAction{
         this.begin = begin;
         this.end = end;
         this.thresh = thresh;
+        this.pivot = (end-begin)/2;
 }
-
     //Override for the compute() method implemented from RecursiveAction
     @Override
     protected void compute() {
+        //If the array provided is smaller than the threshold, then a sequential sort is performed.
         if((end-begin)<= thresh){
             Arrays.sort(arr, begin, end);
         }
+        else{
+            this.pivot = getMedian();
+            //Perform a parallel pack
+            
+        }
+    }
+    
+    
+    /**
+     * A pack algorithm using the bit-bitsum-output method to enable parallelization.
+     */
+    private void pack(int start, int end, int val){
+        int[] bits = new int[(end-start)+1];
+        int[] bitsum = new int[bits.length];
+        for (int i = start; i < end; i++) {
+            if(arr[i]<val){
+                bits[i-start] = 0;
+            }
+            else if(arr[i]>= val){
+                bits[i-start] = 1;
+            }
+        }
+        //Prefix-Sum Algorithm
+        
+    }
+    
+    
+    /**
+     * Estimates the median of the array in O(c) time and returns the index of the median.
+     * @return pivot
+     */
+    private int getMedian(){
+        //Sneaky trick to select a pretty good privot in O(1) time.
+        Random rand = new Random();
+        int[] medianator = new int[3];
+        for (int i = 0; i <3; i++) {
+            medianator[i] = rand.nextInt(end);
+        }
+        int hi = medianator[0];
+        int lo = medianator[0];
+        int piv = medianator[0];
+        for (int i = 0; i < 3; i++) {
+            if(arr[medianator[i]]<= arr[hi] && arr[medianator[i]]>=arr[lo]){
+                piv = medianator[i];
+            }
+            if(arr[medianator[i]]>=arr[hi]){
+                hi = medianator[i];
+            }
+            if(arr[medianator[i]]<=arr[lo]){
+                lo = medianator[i];
+            }
+        }
+        //System.out.println(arr[medianator[0]]+";"+arr[medianator[1]]+";"+arr[medianator[2]]+" :\t"+pivot+"\t"+arr[pivot]);
+        return piv;
     }
     
 }
--- test/TestQuicksortParallel.java
+++ test/TestQuicksortParallel.java
@@ -4,6 +4,8 @@
  * and open the template in the editor.
  */
 
+import java.util.Arrays;
+import java.util.Random;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -19,27 +21,32 @@ public class TestQuicksortParallel {
     
     public TestQuicksortParallel() {
     }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
 
     @Test
-    public void testSomeMethod() {
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testCompute() {
+        final int ARRLEN = 400000;
+        final int THRESH = 200000;
+        Random rand = new Random();
+        int[] sampleArr = {27, 43, 3, 9, 82, 10, 38};
+        QuicksortParallel QSP = new QuicksortParallel(sampleArr, 0, 6, 4);
+        QSP.compute();
+        System.out.println(Arrays.toString(sampleArr)+"\n\n");
+        
+        int[] arr = new int[ARRLEN];
+        for(int i=0; i<ARRLEN; i++){
+            arr[i]=rand.nextInt(ARRLEN);
+        }
+        int[] sysArr = arr.clone();
+        Arrays.sort(sysArr);
+        QuicksortParallel qsp = new QuicksortParallel(arr, 0, ARRLEN, THRESH);
+        qsp.compute();
+        int trueCount = 0;
+        for(int i=0; i<ARRLEN; i++){
+            if(sysArr[i]==arr[i]){trueCount++;}
+        }
+        System.out.println("Correctness: "+(trueCount*100/ARRLEN)+"%");
+        assert(Arrays.equals(sysArr, arr));
     }
+
+ }
     
-}
