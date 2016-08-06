e8ed38fef0a9e80f58b5fd9b3fffa336234f943c
Gianluca Truda <gianlucatruda@gmail.com>
2016-08-06 15:22:32 +0200

    Updated the main method to automate the process of determining the best time from 5 separate runs for each array size. Also added a timeout feature for if a single attempt exceeds 60 seconds the whole instance shuts down. This should protect me from maxing out the machines I test on.

--- src/DriverSort.java
+++ src/DriverSort.java
@@ -14,11 +14,12 @@ public class DriverSort {
     @SuppressWarnings("empty-statement")
     public static void main(String[] args) throws IOException{
         //Initialising important variables and assigning them default values
-        String SORT = "Quicksort";
+        String SORT = "Mergesort";
         int ARRAYSIZEMIN = 1000000;
-        int ARRAYSIZEMAX = 10000000;
-        int ARRAYSIZEINCR = 1000000;
+        int ARRAYSIZEMAX = 31000000;
+        int ARRAYSIZEINCR = 5000000;
         String OUTFILENAME = "data.txt";
+        double THRESHVAL = 0.5;
         
         //Getting the commandline arguments from the user
         try{
@@ -27,52 +28,61 @@ public class DriverSort {
             ARRAYSIZEMAX = Integer.parseInt(args[2]);
             ARRAYSIZEINCR = Integer.parseInt(args[3]);
             OUTFILENAME = args[4];
+            THRESHVAL = Double.parseDouble(args[5]);
         }
         catch (ArrayIndexOutOfBoundsException e) {
-            if(args.length > 0) {System.out.println("Too few parameters! \n <sort> <arraySizeMin> <arraySizeMax> <arraySizeIncr> <outFileName>");}
-            System.out.println("Instance : " + SORT +"; " + ARRAYSIZEMIN +"; " + ARRAYSIZEMAX +"; " + ARRAYSIZEINCR +"; " + OUTFILENAME);
-            writeToFile(OUTFILENAME, SORT +"; " + ARRAYSIZEMIN +"; " + ARRAYSIZEMAX +"; " + ARRAYSIZEINCR +"; " + OUTFILENAME); 
+            if(args.length > 0) {System.out.println("Too few parameters! \n <sort> <arraySizeMin> <arraySizeMax> <arraySizeIncr> <outFileName> <thresholdCoefficient>");}
         }
+        
+        System.out.println("\nInstance : " + SORT +"; " + ARRAYSIZEMIN +"; " + ARRAYSIZEMAX +"; " + ARRAYSIZEINCR +"; " + OUTFILENAME +"; " +THRESHVAL+"\n"+"Time(ms)");
+        writeToFile(OUTFILENAME, "\n"+SORT +"; " + ARRAYSIZEMIN +"; " + ARRAYSIZEMAX +"; " + ARRAYSIZEINCR +"; " + OUTFILENAME +"; "+ THRESHVAL); 
+        
         
         //Variables to implement across all testing
         int[] arr;
         long tick = 0;
         Random rand = new Random();
         int elementSize = 100000;
-        int repeats = 2;
+        int repeats = 5;
         
-        //Code to perform generalised timing, with an imbedded if-net to only perform the user-specified sort on each run
+        //Code to perform generalised timing, with an embedded if-net to only perform the user-specified sort on each run
         for(int arraySize=ARRAYSIZEMIN; arraySize<=ARRAYSIZEMAX; arraySize=arraySize+ARRAYSIZEINCR){
             String outText ="";
+            int best = 100000;
             arr = new int[arraySize];
-            for(int i=0; i<arraySize; i++){
-                arr[i]=rand.nextInt(elementSize);
-            }
-            outText+="Size: "+arraySize+"\tTime(ms):";
             for(int i=0; i<repeats; i++){
+                for(int j=0; j<arraySize; j++){
+                    arr[j]=rand.nextInt(elementSize);
+                }
                 //Garbage collection called to avoid it slowing down the testing
                 System.gc();
+                
                 //If statements to run the appropriate sort based on user parameters from command line
                 if(SORT.equals("Quicksort") || SORT.equals("quicksort")){
-                    QuicksortParallel qsp = new QuicksortParallel(arr,(int)(arraySize/8));
+                    QuicksortParallel qsp = new QuicksortParallel(arr,(int)(THRESHVAL*arraySize));
                     tick = getTime();
                     qsp.compute();
                 }
                 if(SORT.equals("Mergesort") || SORT.equals("mergesort")){
-                    MergesortParallel msp = new MergesortParallel(arr, 0, arraySize, (int)(arraySize/4));
+                    MergesortParallel msp = new MergesortParallel(arr, 0, arraySize, (int)(THRESHVAL*arraySize));
                     tick = getTime();
                     msp.compute();
-                }
-                if(SORT.equals("Altsort") || SORT.equals("altsort")){
-
                 }
                 if(SORT.equals("System") || SORT.equals("system")){
                     tick = getTime();
                     Arrays.sort(arr);
                 }
+                
                 int duration = (int)(getTime() - tick);
-                outText+="\t"+duration;
+                if(duration > 60000){
+                    best = 60000;
+                    System.exit(duration);
+                }
+                if(duration < best){
+                    best = duration;
+                }
             }
+            outText+= best;
             System.out.println(outText);
             writeToFile(OUTFILENAME, outText);
         }
